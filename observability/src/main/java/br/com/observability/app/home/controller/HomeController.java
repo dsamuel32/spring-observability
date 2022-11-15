a28d0b0a14a4b1b7aa5ea335603d668b9328d8c1
dsamuel32 <diego.samuel.alves@gmail.com>
2022-11-15 11:14:27 -0300

    Add log config

--- observability/src/main/java/br/com/observability/app/home/controller/HomeController.java
+++ observability/src/main/java/br/com/observability/app/home/controller/HomeController.java
@@ -1,6 +1,7 @@
 package br.com.observability.app.home.controller;
 
 import br.com.observability.app.home.response.HomeResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -13,11 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(
         value = "v1/home",
         produces = MediaType.APPLICATION_JSON_VALUE)
+@Slf4j
 public class HomeController {
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public HomeResponse getHomeInfo() {
+        //https://www.baeldung.com/java-application-logs-to-elastic-stack
+        log.info("Getting info");
         return new HomeResponse("Running...");
     }
 
