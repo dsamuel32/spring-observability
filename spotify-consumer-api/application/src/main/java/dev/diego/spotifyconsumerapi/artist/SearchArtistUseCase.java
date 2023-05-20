f175ddc897ff7f6ecdd58e4d4887bd942e959459
dsamuel32 <diego.samuel.alves@gmail.com>
2023-05-20 13:53:36 -0300

    Criando testes consulta de artista use case.

--- spotify-consumer-api/application/src/main/java/dev/diego/spotifyconsumerapi/artist/SearchArtistUseCase.java
+++ spotify-consumer-api/application/src/main/java/dev/diego/spotifyconsumerapi/artist/SearchArtistUseCase.java
@@ -9,14 +9,15 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @RequiredArgsConstructor
-public class SearchArtistUseCase implements UseCase<Search, Pageable<ArtistOutput>> {
+public class SearchArtistUseCase implements UseCase<SearchArtistInput, Pageable<ArtistOutput>> {
 
     private final Logger LOGGER = Logger.getLogger(SearchArtistUseCase.class.getName());
 
     private final ArtistGateway gateway;
     @Override
-    public Pageable<ArtistOutput> execute(final Search search) {
-        LOGGER.log(Level.INFO, "[SEARCH ARTIST][START] - {}", search);
+    public Pageable<ArtistOutput> execute(final SearchArtistInput input) {
+        LOGGER.log(Level.INFO, "[SEARCH ARTIST][START] - {}", input);
+        final var search = Search.with(input.artistName(), input.itemsPerPage(), input.pageNumber());
         final var result = gateway.search(search);
         LOGGER.log(Level.INFO, "[SEARCH ARTIST][END] - Found: {}", result.getTotal());
         return result.map(ArtistOutput::from);
---
+++ spotify-consumer-api/application/src/test/java/dev/diego/spotifyconsumerapi/artist/SearchArtistUseCaseTest.java
@@ -0,0 +1,94 @@
+package dev.diego.spotifyconsumerapi.artist;
+
+import dev.diego.spotifyconsumerapi.Pageable;
+import dev.diego.spotifyconsumerapi.Search;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class SearchArtistUseCaseTest {
+
+    @InjectMocks
+    private SearchArtistUseCase useCase;
+
+    @Mock
+    private ArtistGateway gateway;
+
+    @Test
+    void given_aSearchInputWithArtistNameField_returnPageableArtistEmpty() {
+
+        when(gateway.search(any(Search.class))).thenReturn(Pageable.with(Collections.emptyList(), 1, 0, 0));
+        final var input = SearchArtistInput.with("band 1", null, null);
+        final var result = useCase.execute(input);
+
+        final var expected = Pageable.with(Collections.emptyList(), 1, 0, 0);
+
+        assertEquals(expected, result);
+        verify(gateway).search(Search.with("band 1", 100, 0));
+
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+            value = {"band 1, 1, 1", "band 1, 1, 2"}
+    )
+    void given_aSearchInputWithArtistNameAndPageNumberAndItemsPerPage_returnPageableArtistPage(
+            final String name, final Integer itemsPerPage, final Integer pageNumber) {
+
+        final var artist = createArtist();
+
+        when(gateway.search(any(Search.class))).thenReturn(createPageableArtist(artist, pageNumber));
+        final var input = SearchArtistInput.with(name, itemsPerPage, pageNumber);
+        final var result = useCase.execute(input);
+
+        final var expected = createPageableArtistOutput(artist, pageNumber);
+
+        assertEquals(expected, result);
+        verify(gateway).search(Search.with(name, itemsPerPage, pageNumber));
+
+    }
+
+    private Artist createArtist() {
+        return Artist.with(
+                "band 1",
+                1000L,
+                10L,
+                "spotfy:fake.uri",
+                Set.of("Rock", "Metal"),
+                Set.of(Image.with(10, "spotfy:fake.uri", 10))
+        );
+    }
+
+    private Pageable<Artist> createPageableArtist(final Artist artist, final Integer pageNumber) {
+        return Pageable.with(
+                List.of(artist),
+                1,
+                pageNumber,
+                10
+        );
+    }
+
+    private Pageable<ArtistOutput> createPageableArtistOutput(final Artist artist, final Integer pageNumber) {
+        return Pageable.with(
+                List.of(ArtistOutput.from(artist)),
+                1,
+                pageNumber,
+                10
+        );
+    }
+
+}
